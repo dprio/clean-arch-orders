e5fe0ffacf36c6755f7eef557cd01fd3d332ab55
Daniel <daniel.pedroso94@gmail.com>
2025-11-23 17:37:08 +0000

    feat: injecting repository into useCase

--- internal/usecase/usecases.go
+++ internal/usecase/usecases.go
@@ -4,16 +4,19 @@ import (
 	"github.com/dprio/clean-arch-orders/internal/domain/eventtype"
 	"github.com/dprio/clean-arch-orders/internal/infrastructure/db"
 	"github.com/dprio/clean-arch-orders/internal/usecase/createorder"
+	"github.com/dprio/clean-arch-orders/internal/usecase/getorders"
 	"github.com/dprio/clean-arch-orders/pkg/events"
 )
 
 type UseCases struct {
 	CreateOrderUseCase createorder.UseCase
+	GetOrdersUseCase   getorders.UseCase
 }
 
 func New(dbs *db.DBs, eventDispatcher events.EventDispatcherInterface) *UseCases {
 	return &UseCases{
 		CreateOrderUseCase: buildCreateOrderUseCase(dbs, eventDispatcher),
+		GetOrdersUseCase:   getorders.New(dbs.OrderRepository),
 	}
 }
 
