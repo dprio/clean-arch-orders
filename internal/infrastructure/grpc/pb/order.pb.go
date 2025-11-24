840219ea9320750f79eb06ef33e3a8baf157bc0a
Daniel <daniel.pedroso94@gmail.com>
2025-11-24 20:06:17 -0300

    feat: get orders grpc service (#10)

--- internal/infrastructure/grpc/pb/order.pb.go
+++ internal/infrastructure/grpc/pb/order.pb.go
@@ -141,6 +141,86 @@ func (x *CreateOrderResponse) GetFinalPrice() float32 {
 	return 0
 }
 
+type OrdersResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Orders        []*CreateOrderResponse `protobuf:"bytes,1,rep,name=orders,proto3" json:"orders,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *OrdersResponse) Reset() {
+	*x = OrdersResponse{}
+	mi := &file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes[2]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *OrdersResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*OrdersResponse) ProtoMessage() {}
+
+func (x *OrdersResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes[2]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use OrdersResponse.ProtoReflect.Descriptor instead.
+func (*OrdersResponse) Descriptor() ([]byte, []int) {
+	return file_internal_infrastructure_grpc_protofiles_order_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *OrdersResponse) GetOrders() []*CreateOrderResponse {
+	if x != nil {
+		return x.Orders
+	}
+	return nil
+}
+
+type Empty struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *Empty) Reset() {
+	*x = Empty{}
+	mi := &file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes[3]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *Empty) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Empty) ProtoMessage() {}
+
+func (x *Empty) ProtoReflect() protoreflect.Message {
+	mi := &file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes[3]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Empty.ProtoReflect.Descriptor instead.
+func (*Empty) Descriptor() ([]byte, []int) {
+	return file_internal_infrastructure_grpc_protofiles_order_proto_rawDescGZIP(), []int{3}
+}
+
 var File_internal_infrastructure_grpc_protofiles_order_proto protoreflect.FileDescriptor
 
 const file_internal_infrastructure_grpc_protofiles_order_proto_rawDesc = "" +
@@ -154,9 +234,13 @@ const file_internal_infrastructure_grpc_protofiles_order_proto_rawDesc = "" +
 	"\x05price\x18\x02 \x01(\x02R\x05price\x12\x10\n" +
 	"\x03tax\x18\x03 \x01(\x02R\x03tax\x12\x1f\n" +
 	"\vfinal_price\x18\x04 \x01(\x02R\n" +
-	"finalPrice2N\n" +
+	"finalPrice\"A\n" +
+	"\x0eOrdersResponse\x12/\n" +
+	"\x06orders\x18\x01 \x03(\v2\x17.pb.CreateOrderResponseR\x06orders\"\a\n" +
+	"\x05Empty2z\n" +
 	"\fOrderService\x12>\n" +
-	"\vCreateOrder\x12\x16.pb.CreateOrderRequest\x1a\x17.pb.CreateOrderResponseB!Z\x1finternal/infrastructure/grpc/pbb\x06proto3"
+	"\vCreateOrder\x12\x16.pb.CreateOrderRequest\x1a\x17.pb.CreateOrderResponse\x12*\n" +
+	"\tGetOrders\x12\t.pb.Empty\x1a\x12.pb.OrdersResponseB!Z\x1finternal/infrastructure/grpc/pbb\x06proto3"
 
 var (
 	file_internal_infrastructure_grpc_protofiles_order_proto_rawDescOnce sync.Once
@@ -170,19 +254,24 @@ func file_internal_infrastructure_grpc_protofiles_order_proto_rawDescGZIP() []by
 	return file_internal_infrastructure_grpc_protofiles_order_proto_rawDescData
 }
 
-var file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_internal_infrastructure_grpc_protofiles_order_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_internal_infrastructure_grpc_protofiles_order_proto_goTypes = []any{
 	(*CreateOrderRequest)(nil),  // 0: pb.CreateOrderRequest
 	(*CreateOrderResponse)(nil), // 1: pb.CreateOrderResponse
+	(*OrdersResponse)(nil),      // 2: pb.OrdersResponse
+	(*Empty)(nil),               // 3: pb.Empty
 }
 var file_internal_infrastructure_grpc_protofiles_order_proto_depIdxs = []int32{
-	0, // 0: pb.OrderService.CreateOrder:input_type -> pb.CreateOrderRequest
-	1, // 1: pb.OrderService.CreateOrder:output_type -> pb.CreateOrderResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	1, // 0: pb.OrdersResponse.orders:type_name -> pb.CreateOrderResponse
+	0, // 1: pb.OrderService.CreateOrder:input_type -> pb.CreateOrderRequest
+	3, // 2: pb.OrderService.GetOrders:input_type -> pb.Empty
+	1, // 3: pb.OrderService.CreateOrder:output_type -> pb.CreateOrderResponse
+	2, // 4: pb.OrderService.GetOrders:output_type -> pb.OrdersResponse
+	3, // [3:5] is the sub-list for method output_type
+	1, // [1:3] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_internal_infrastructure_grpc_protofiles_order_proto_init() }
@@ -196,7 +285,7 @@ func file_internal_infrastructure_grpc_protofiles_order_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_internal_infrastructure_grpc_protofiles_order_proto_rawDesc), len(file_internal_infrastructure_grpc_protofiles_order_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- internal/infrastructure/grpc/service/order_service.go
+++ internal/infrastructure/grpc/service/order_service.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/dprio/clean-arch-orders/internal/infrastructure/grpc/pb"
 	"github.com/dprio/clean-arch-orders/internal/usecase/createorder"
+	"github.com/dprio/clean-arch-orders/internal/usecase/getorders"
 )
 
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase createorder.UseCase
+	GetOrdersUseCase   getorders.UseCase
 }
 
-func NewOrderService(createOrderUseCase createorder.UseCase) pb.OrderServiceServer {
+func NewOrderService(createOrderUseCase createorder.UseCase, getOrdersUseCase getorders.UseCase) pb.OrderServiceServer {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		GetOrdersUseCase:   getOrdersUseCase,
 	}
 }
 
@@ -32,5 +35,26 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Price:      float32(output.Price),
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
+
+func (s *OrderService) GetOrders(ctx context.Context, _ *pb.Empty) (*pb.OrdersResponse, error) {
+	output, err := s.GetOrdersUseCase.Execute(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	ordersResponse := make([]*pb.CreateOrderResponse, len(output))
+	for i, out := range output {
+		ordersResponse[i] = &pb.CreateOrderResponse{
+			Id:         out.ID,
+			Price:      float32(out.Price),
+			Tax:        float32(out.Tax),
+			FinalPrice: float32(out.FinalPrice),
+		}
+	}
+
+	return &pb.OrdersResponse{
+		Orders: ordersResponse,
 	}, nil
 }
